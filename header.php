265c374ac63ea4a7601d5fc47056fe2489b7aed1
Daniel Griffiths <daniel.griffiths.jobs@gmail.com>
2016-02-10 22:34:31 +0000

    added favicon link to the header

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
 	<title><?php wp_title( ' | ', true, 'right' ); ?></title>
 	<link rel="stylesheet" type="text/css" href="<?= get_template_directory_uri() ?>/assets/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?= get_template_directory_uri() ?>/assets/css/mobile.css" />
+	<link rel="shortcut icon" href="<?= get_stylesheet_directory_uri(); ?>/assets/img/favicon.ico" />
 	<!--[if lt IE 9]>
 	  <script type="text/javascript" src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
